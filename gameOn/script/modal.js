9b2dc7535aab37c443e336a092e65e30a1e904c8
B-Audrey <audrey@devzone.fr>
2023-09-19 16:01:50 +0200

    ouverture et fermeture modale

---
+++ gameOn/script/modal.js
@@ -0,0 +1,54 @@
+function editNav() {
+    const x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const modalCloseX = document.querySelector(".close");
+const mainContent = document.querySelector(".hero-section");
+const submitBtn = document.querySelector('.btn-submit');
+const modalCloseBtn = document.querySelector(".btn-close");
+const validationMessage = document.querySelector(".validation-message");
+
+
+// launch modal form
+// display the white background and hide the back content
+const launchModal = () => {
+    modalbg.style.display = "block";
+    mainContent.style.opacity = "0";
+    modalCloseBtn.style.display = "none";
+    submitBtn.style.display = "block";
+    formData.forEach((e) => e.style.display = "block");
+}
+
+//close modal event
+// reverse the opening function
+const closeModal = () => {
+    modalbg.style.display = "none";
+    mainContent.style.opacity = "1";
+    validationMessage.style.display = "none";
+}
+
+// on submit display message, close btn and hide the form
+const sendSubmitMessage = () => {
+    formData.forEach((e) => e.style.display = "none");
+    submitBtn.style.display = "none";
+    modalCloseBtn.style.display = "block";
+    validationMessage.style.display = "flex";
+}
+
+// launch modal event, reacting when modal is opening or closing
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalCloseX.addEventListener("click", closeModal);
+modalCloseBtn.addEventListener("click", closeModal)
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    sendSubmitMessage()
+})
